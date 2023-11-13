4b5dcb787ff5e94c17d2655738e18e7af1ca8d77
heisfer <heisfer@refract.dev>
2023-11-13 22:48:22 +0200

    changed devenv.nix to example

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,10 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  packages = [ pkgs.hello ];
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
+  scripts.say-bye.exec = ''
+    echo bye
   '';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
+
